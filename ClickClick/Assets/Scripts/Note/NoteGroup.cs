a78908db0f8c493f68908d894e1f3aaa9d01965b
kimhyeok <kimmang940715@naver.com>
2024-04-15 01:59:51 +0900

    feat : 노트 생성 기능 구현

--- ClickClick/Assets/Scripts/Note/NoteGroup.cs
+++ ClickClick/Assets/Scripts/Note/NoteGroup.cs
@@ -6,7 +6,11 @@ public class NoteGroup : MonoBehaviour
 {
     [SerializeField] private int noteMaxNum = 5;
     [SerializeField] private Note baseNoteClass = null;
+    
+    [SerializeField] private Transform noteSpawnTrf = null;
     private List<Note> noteClassList;
+
+    private float noteGapInterval = 8f;
 
     private void Awake()
     {
@@ -16,10 +20,16 @@ public class NoteGroup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var _noteClassObj = GameObject.Instantiate(this.baseNoteClass.gameObject);
-        var _noteClass = _noteClassObj.GetComponent<Note>();
+        for (int i = 0; i < this.noteMaxNum; i++)
+        {
+            var _noteClassObj = GameObject.Instantiate(this.baseNoteClass.gameObject);
+            _noteClassObj.transform.SetParent(this.noteSpawnTrf);
+            _noteClassObj.transform.localPosition = Vector3.up * this.noteClassList.Count * this.noteGapInterval;
+            var _noteClass = _noteClassObj.GetComponent<Note>();
 
-        this.noteClassList.Add( _noteClass );
+            this.noteClassList.Add(_noteClass);
+        }
+
     }
 
     // Update is called once per frame
