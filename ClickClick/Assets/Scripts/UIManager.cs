5d206a34ed953ccf14e4394d0b01782bac00db89
kimhyeok <kimmang940715@naver.com>
2024-04-16 20:02:40 +0900

    feat : 점수관리를 위한 UI매니저 생성

---
+++ ClickClick/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public static UIManager Instance;
+
+    [SerializeField] private Image scroeImg = null;
+    [SerializeField] private TextMeshProUGUI scoreTmp = null;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OnScore(int _currentScore, int _maxScore)
+    {
+        this.scoreTmp.text = $"{_currentScore}/{_maxScore}";
+
+        this.scroeImg.fillAmount = (float)_currentScore / _maxScore;
+    }
+}
